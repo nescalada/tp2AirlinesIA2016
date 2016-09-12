477b8d6802aa85a7568c92614a4b670c795e65c3
nescalada <nehuenescalada@gmail.com>
2016-09-12 19:23:02 -0300

    Se leen los echos y reglas desde un archivo pl, se deja ejemplo en este commit

--- tp2IAoceanicAirlines2016/src/tp2IAoceanicAirlines2016/PrincipalGUI.java
+++ tp2IAoceanicAirlines2016/src/tp2IAoceanicAirlines2016/PrincipalGUI.java
@@ -4,24 +4,25 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import org.cs3.prolog.connector.Connector;
 import org.cs3.prolog.connector.common.QueryUtils;
+import org.cs3.prolog.connector.process.PrologException;
 import org.cs3.prolog.connector.process.PrologProcess;
 import org.cs3.prolog.connector.process.PrologProcessException;
 
 import javax.swing.JComboBox;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.Map;
-import java.awt.event.ActionEvent;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
 
-public class GUIprincipal {
+public class PrincipalGUI {
 
 	private JFrame frame;
 
@@ -32,7 +33,7 @@ public class GUIprincipal {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GUIprincipal window = new GUIprincipal();
+					PrincipalGUI window = new PrincipalGUI();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -44,7 +45,7 @@ public class GUIprincipal {
 	/**
 	 * Create the application.
 	 */
-	public GUIprincipal() {
+	public PrincipalGUI() {
 		initialize();
 	}
 
@@ -53,8 +54,8 @@ public class GUIprincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(GUIprincipal.class.getResource("/tp2IAoceanicAirlines2016/Airplane.png")));
-		frame.setBounds(100, 100, 327, 324);
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(PrincipalGUI.class.getResource("/tp2IAoceanicAirlines2016/Recursos/Airplane.png")));
+		frame.setBounds(100, 100, 312, 282);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -66,19 +67,20 @@ public class GUIprincipal {
 		lblDestino.setBounds(15, 77, 69, 20);
 		frame.getContentPane().add(lblDestino);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(110, 24, 167, 26);
-		frame.getContentPane().add(comboBox);
+		JComboBox cmboxOrigen = new JComboBox();
+		cmboxOrigen.setBounds(110, 24, 167, 26);
+		frame.getContentPane().add(cmboxOrigen);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(110, 74, 167, 26);
-		frame.getContentPane().add(comboBox_1);
+		JComboBox cmboxDestino = new JComboBox();
+		cmboxDestino.setBounds(110, 74, 167, 26);
+		frame.getContentPane().add(cmboxDestino);
 		
 		JCheckBox chckbxPrefieroPocosTransbordos = new JCheckBox("Prefiero pocos transbordos");
-		chckbxPrefieroPocosTransbordos.setBounds(20, 119, 257, 29);
+		chckbxPrefieroPocosTransbordos.setBounds(15, 119, 262, 29);
 		frame.getContentPane().add(chckbxPrefieroPocosTransbordos);
 		
-		JButton btnsolicitarRecomendacin = new JButton("\u00A1Solicitar recomendaci\u00F3n!");
+		JButton btnsolicitarRecomendacin = new JButton("  \u00A1Solicitar recomendaci\u00F3n!");
+		btnsolicitarRecomendacin.setIcon(new ImageIcon(PrincipalGUI.class.getResource("/tp2IAoceanicAirlines2016/Recursos/Airplane.png")));
 		btnsolicitarRecomendacin.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -96,7 +98,7 @@ public class GUIprincipal {
             PrologProcess process = Connector.newPrologProcess();
 
             //Establece la base de echos
-            fillFactbaseWithDemoData(process);
+            cargarBaseDeEchosYReglas(process);
 
             // Crea una consulta con el método buildTerm
             // esto es lo mismo que "father_of(Father, peter)"
@@ -123,22 +125,23 @@ public class GUIprincipal {
                 //Itera sobre cada resultado
                 System.out.println(r.get("Child") + " is a child of john");
             }
-        } catch (Exception e) {
+        } catch (PrologException a){
+        	// TODO Auto-generated catch block
+        				JOptionPane.showMessageDialog(null, "Verifique:\nLa existencia de su archivo .pl en el classpath de la aplicación.\nLa existencia de echos y reglas en su archivo .pl.\nQue swiprolog esté actualizado.", "ERROR AL INTENTAR LEER ARCHIVO PROLOG", JOptionPane.ERROR_MESSAGE);
+        }catch (Exception e) {
             e.printStackTrace();
         }
 	}
 	
-	private static void fillFactbaseWithDemoData(PrologProcess process) throws PrologProcessException {
-		// base de echos
-		
-		// this can be done by asserting facts directly
-        process.queryOnce("assertz(father_of(paul, peter))");
+	private static void cargarBaseDeEchosYReglas(PrologProcess process) throws PrologProcessException {		
+		// se pueden escribir directamente
+        //process.queryOnce("assertz(father_of(paul, peter))");
         //process.queryOnce("assertz(father_of(john, paul))");
         //process.queryOnce("assertz(father_of(john, ringo))");
         //process.queryOnce("assertz(father_of(john, george))");
 
-        // or by consulting a file
-        // String consultQuery = QueryUtils.bT("reconsult", "'c:/some_prolog_file.pl'");
-        // process.queryOnce(consultQuery);
+		//o consultando a un archivo
+        String consultQuery = QueryUtils.bT("reconsult", "'src/tp2IAoceanicAirlines2016/Recursos/vuelosbe_origen-destino-tiempomin.pl'");
+        process.queryOnce(consultQuery);
     }
 }
